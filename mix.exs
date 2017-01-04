2cfb48b6d917870e76fd8b3fbab37687bcbe5fd7
Anton Mishchuk <anton.mishchuk@gmail.com>
2017-01-05 00:00:28 +0200

    Add 1.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Flowex.Mixfile do
   def project do
     [app: :flowex,
      version: "0.1.0",
-     elixir: ">= 1.2.0",
+     elixir: ">= 1.3.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      preferred_cli_env: [espec: :test],
