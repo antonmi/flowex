efd7de683196868bab185271c01e055999fc558f
Anton Mishchuk <anton.mishchuk@gmail.com>
2017-01-04 23:49:26 +0200

    Prepare to release

--- mix.exs
+++ mix.exs
@@ -4,33 +4,40 @@ defmodule Flowex.Mixfile do
   def project do
     [app: :flowex,
      version: "0.1.0",
-     elixir: "~> 1.4-dev",
+     elixir: ">= 1.2.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      preferred_cli_env: [espec: :test],
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package(),
+     source_url: "https://github.com/antonmi/flowex"
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:gen_stage, "0.10.0"},
       {:espec, "~> 1.2.1", only: :test}
     ]
+  end
+
+  defp description do
+    """
+      Flow-Based Programming with Elixir GenStage.
+    """
+  end
+
+  defp package do
+   [
+     files: ~w(lib mix.exs README.md),
+     maintainers: ["Anton Mishchuk"],
+     licenses: ["MIT"],
+     links: %{"github" => "https://github.com/antonmi/flowex"}
+   ]
   end
 end
