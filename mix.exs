a75ad6fc02c47d79903c0815a31c22107ef9812c
Anton Mishchuk <anton.mishchuk@gmail.com>
2017-06-19 22:53:12 +0300

    Version 0.5.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Flowex.Mixfile do
 
   def project do
     [app: :flowex,
-     version: "0.4.0",
+     version: "0.5.0",
      elixir: ">= 1.3.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
